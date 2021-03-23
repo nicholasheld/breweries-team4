8696117a01460a48968adb18baa441b67899efa9
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-03-23 05:26:37 +0300

    add js code

--- develop.js
+++ develop.js
@@ -1,3 +1,40 @@
-/*
-Javascript
-*/
+// When I search a 5-digit zip code 
+// Then I am presented with a list of breweries and restaurants in that zip code
+
+// define variables
+// var searchButton = $("#search-btn")
+// var userInput = $("#user-input")
+// var breweryList = [];
+// var foodList = [];
+// 
+
+// Function to get data from brewery API
+// function getBrewery(event)
+// event.prevent default()
+// var postalCode = get the value from the zip code text input
+// var postalURL = 'https://api.openbrewerydb.org/breweries/search?query=${postalCode}&per_page=10&sort=-name'
+// fetch()
+
+// Function to get data from restaurant API
+// prevent default
+// post function
+
+// On click event for search button
+// var searchList = /*html*/ $(' write html here ')
+// Append list dynamically to show results
+// if (results are empty){
+//   Text(no breweries were found for this zip code)
+// }
+
+// Local storage for previous search
+// $(document).ready()
+
+
+
+
+/* Extras */
+// https://api.openbrewerydb.org/breweries/autocomplete?query=${postalCode}&per_page=10&sort=-name
+// https://api.openbrewerydb.org/breweries?by_type=${type}
+
+
+// To start the search for breweries
