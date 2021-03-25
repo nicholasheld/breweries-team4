a3aa36756e30f18980f0006f154da88342d97639
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-03-25 05:34:05 +0300

    organize HTML - add search button function

--- develop.js
+++ develop.js
@@ -11,43 +11,45 @@ var breweryList = [];
 // 
 
 // Function to get data from brewery API
-// function getBrewery(event)
-// event.prevent default()
-// var postalCode = get the value from the zip code text input
-// var postalURL = 'https://api.openbrewerydb.org/breweries/search?query=${postalCode}&per_page=10&sort=-name'
-// fetch()
+function getBrewery() {
+
+    $.ajax({
+        URL: 'https://api.openbrewerydb.org/breweries/search?query=${postalCode}&per_page=10&sort=-name'
+    })
+
+    // var postalCode = get the value from the zip code text input
+
+}
 
 // Function to get data from restaurant API
 // prevent default
 // post function
 
 // On click event for search button
-    $("#search-btn").on("click", function(event) {
-        event.preventDefault();
-        console.log("hello")
+$("#search-btn").on("click", function(event) {
+    event.preventDefault();
+    console.log("hello")
+    
+    var userInputVal = $(userInput).val().trim();
 
-        var userInputVal = $(userInput).val().trim();
-        var searchList = html`
+    var searchList = html`
         <div class="card-info">
             <h1 class="card-title">Card Title</h1>
             <p class="card-author">Author Name</p>
         </div>`;
-        /* var divEl = document.createElement(
-            divEl.setAttr("id", "brewery-info")
-        );
-        var card = $("#brewery-info");
-        divEl.textContent = ("Hello World"); */
-        $("#brewery-info").append(searchList);
-    });
-// Append list dynamically to show results
-// if (results are empty){
-//   Text(no breweries were found for this zip code)
-// }
+
+    $("#brewery-info").append(searchList);
+    // Prevent continual appending of the search result
+    // if (results are empty){
+    //   Text(no breweries were found for this zip code)
+    // }
+});
 
 
 
 
-// Local storage for previous search
+
+// Local storage to show last index or append a zip code list dynamically
 // $(document).ready()
 
 
@@ -56,6 +58,4 @@ var breweryList = [];
 /* Extras 
 https://api.openbrewerydb.org/breweries/autocomplete?query=${postalCode}&per_page=10&sort=-name
 https://api.openbrewerydb.org/breweries?by_type=${type}
-
-
-To start the search for breweries */
+*/
