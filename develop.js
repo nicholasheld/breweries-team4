b917fa59877b9f8842f3ee99b489755273b146f9
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-03-27 00:54:32 +0300

    Add get brewery function

--- develop.js
+++ develop.js
@@ -11,11 +11,74 @@ var breweryList = [];
 // 
 
 // Function to get data from brewery API
-function getBrewery() {
+function getBrewery(postalCode) {
+    $("#brewery-info").empty();
+    var settings = {
+        "url": `https://api.openbrewerydb.org/breweries?by_postal=${postalCode}&per_page=10`,
+        "method": "GET",
+        "timeout": 0,
+    };
+      
+    $.ajax(settings).done(function (response) {
 
+    if(response){
+        console.log(response)
+      
+            for (var i = 0; i < response.length; i++) {
+      
+                var searchList = html`
+                    <div class="card-info">
+                        <h1 class="card-title">${response[i].name}</h1>
+                        <p class="card-location">${response[i].street}, ${response[i].city}, ${response[i].state}</p>
+                        <p class="card-location">Tel. ${response[i].phone.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3')}</p>
+                        <a class="card-location" href="${response[i].website_url}" target="_blank">${response[i].website_url}</a>
+                    </div>`;
+        
+                $("#brewery-info").append(searchList);
+            } 
+    } else {
+        var noResult = "No Search Results Found";
+        var noResultSpan = $("<span>");
+        noResultSpan.text(noResult);
+        $("#brewery-info").append(noResultSpan)
+    }
+      
+});
+
+/*
     $.ajax({
-        URL: 'https://api.openbrewerydb.org/breweries/search?query=${postalCode}&per_page=10&sort=-name'
-    })
+        RequestURL: `https://api.openbrewerydb.org/breweries/search?query=55303&per_page=10&sort=-name`,
+        method: "GET",
+    }).then(function(options) {
+
+        console.log("hello");
+        
+        $("#brewery-info").empty();
+
+        var info = {
+            name: options[0].name,
+            location: options.location,
+        }
+
+        
+        var searchList = html`
+            <div class="card-info">
+                <h1 class="card-title">${info.name}</h1>
+                <p class="card-author">Author Name</p>
+            </div>`;
+        
+
+        var searchList = $(`
+        <div class="card-info">
+            <h1 class="card-title">${info.name}</h1>
+            <p class="card-author">Author Name</p>
+        </div>`);
+
+        $("#brewery-info").append(searchList);
+
+    })*/
+
+
 
     // var postalCode = get the value from the zip code text input
 
@@ -28,17 +91,10 @@ function getBrewery() {
 // On click event for search button
 $("#search-btn").on("click", function(event) {
     event.preventDefault();
-    console.log("hello")
-    
-    var userInputVal = $(userInput).val().trim();
 
-    var searchList = html`
-        <div class="card-info">
-            <h1 class="card-title">Card Title</h1>
-            <p class="card-author">Author Name</p>
-        </div>`;
+    var userInputVal = userInput.val().trim();
 
-    $("#brewery-info").append(searchList);
+    getBrewery(userInputVal);
     // Prevent continual appending of the search result
     // if (results are empty){
     //   Text(no breweries were found for this zip code)
